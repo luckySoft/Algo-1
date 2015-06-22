63192028c22172f91c1f9cfc4d127a75d46e6eaf
luckySoft <evgeni_900@abv.bg>
2015-06-22 18:36:10 +0300

    task ready

--- week3/2-Min-Max-Heap/MinMaxHeap.java
+++ week3/2-Min-Max-Heap/MinMaxHeap.java
@@ -1,3 +1,5 @@
+package MinMaxHeap;
+
 public class MinMaxHeap {
 
   public static class Node {
@@ -5,10 +7,57 @@ public class MinMaxHeap {
     public int value;
     public Node left;
     public Node right;
+    
+    public Node(int value){
+    	this.value = value;
+    	left = null;
+    	right = null;
+    }
+    
   }
 
   // Checks if a binary tree is a min/max heap.
-  public boolean isMinMax(Node root) {
-    // ...
+  private static boolean isMinMax(Node root,boolean level) {
+	  if(root != null && (root.left != null && root.right != null)){
+		  if(level){
+			  if(!(root.left.value > root.value && isMinMax(root.left,!level))){
+					return false;
+				}
+			  if(!(root.right.value > root.value && isMinMax(root.right,!level))){
+					return false;
+			  }
+		  }
+		  else{
+			  if(!(root.left.value < root.value && isMinMax(root.left,!level))){
+					return false;
+				}
+			  if(!(root.right.value < root.value && isMinMax(root.right,!level))){
+					return false;
+				}
+		  }
+	  }
+	  return true;
+  }
+  
+  public static boolean isMinMax(Node root){
+	  
+	  if(isMinMax(root,false)){
+		  return true;
+	  }
+	  else return false;
+  }
+  
+  public static void main(String[] args) {
+	
+	  Node start = new Node(5);
+	  start.left = new Node(1);
+	  start.right = new Node(1);
+	  start.left.left = new Node(11);
+	  start.left.right = new Node(11);
+	  start.right.left = new Node(12);
+	  start.right.right = new Node(12);
+	  System.out.println(isMinMax(start));
+	  
   }
 }
+
